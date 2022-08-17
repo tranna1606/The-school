f34f95ac4a67d4c1eb40129194fb03ddc53654b3
tranna1606 <109428681+tranna1606@users.noreply.github.com>
2022-08-17 21:13:21 +0700

    commit
    zo lai

---
+++ assets/js/tuyensinh.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('.experience-list').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        arrows : false,
+    });
+})
