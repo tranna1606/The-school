65b833abd6f16b93521fa73248430e09d2ef5122
Đỗ Chí Hưng <61616410+dochihung2909@users.noreply.github.com>
2022-08-23 00:17:11 +0700

    responsive siêu siêu

--- assets/js/trangchu.js
+++ assets/js/trangchu.js
@@ -33,4 +33,18 @@ $(document).ready(function() {
     modal.removeClass('open');
     $('body').css('overflow','');   
   })
+
+  $('.mobile-menu-link').click(function() {
+    if (!$('.mobile-menu-list').hasClass('active-mobile-menu')) {
+      $('.mobile-menu-list').addClass('active-mobile-menu');
+      $('.header-scroll').css('background-color','#003057');
+      $('.header-name').css('color','white'); 
+      $(this).children('i').attr('class','fa-solid fa-x')
+    } else {
+      $('.mobile-menu-list').removeClass('active-mobile-menu');
+      $('.header-scroll').css('background-color',''); 
+      $('.header-name').css('color',''); 
+      $(this).children('i').attr('class','fa-solid fa-bars') 
+    }
+  })
 })
--- assets/js/tuyensinh.js
+++ assets/js/tuyensinh.js
@@ -15,6 +15,16 @@ $(document).ready(function() {
         slidesToScroll: 3,
         prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
         nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+        responsive: [
+          {
+            breakpoint: 739,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1, 
+              arrows : false,
+            }
+          },
+        ]
     }); 
 
     $('.question-item-right>i').click(function() { 
